01a22a0108e588f838aea881c43c086fd6700f7a
rakshit1504 <125874320+rakshit1504@users.noreply.github.com>
2024-03-16 15:34:05 +0530

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -5,3 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
         ride: 'carousel'
     })
 });
+
+
+//for video carousel:
+const videosContainer = document.querySelector('.video-container');
+        const videos = document.querySelectorAll('.video-container video');
+        const prevButton = document.querySelector('.prev');
+        const nextButton = document.querySelector('.next');
+        let currentIndex = 0;
+
+        function showVideo(index) {
+            const offset = index * -100 + '%';
+            videosContainer.style.transform = `translateX(${offset})`;
+        }
+
+        function nextVideo() {
+            currentIndex = (currentIndex + 1) % videos.length;
+            showVideo(currentIndex);
+        }
+
+        function prevVideo() {
+            currentIndex = (currentIndex - 1 + videos.length) % videos.length;
+            showVideo(currentIndex);
+        }
+
+        // Add event listeners to arrow buttons
+        prevButton.addEventListener('click', prevVideo);
+        nextButton.addEventListener('click', nextVideo);
